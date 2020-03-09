82f81024c203e9d9dc00402bec190de2fced0d65
jianhan <jianhan@mail.com>
2020-03-09 11:02:00 +0000

    Deploying to gh-pages from  - ee75c19693a10f7e235e625606e3e89789954a31 🚀

--- operations.d.ts
+++ operations.d.ts
@@ -5,8 +5,9 @@ import * as moment from "moment";
 import { Observable } from "rxjs";
 import { Logger } from "winston";
 import { Environment } from "./constants";
+import * as immutable from "immutable";
 /**
- * prefixDateTime prefix any string with date and time for consist displayingsuch as file ame.
+ * prefixDateTime prefix any string with date and time for consist displaying as file ame.
  * @param ft
  */
 export declare const prefixDateTime: (ft: string) => (name: string, dateTime: moment.Moment) => Either<Error, string>;
@@ -19,17 +20,69 @@ export declare const prefixDateTime: (ft: string) => (name: string, dateTime: mo
  */
 export declare const simpleLog: (logger: Logger, level: string, message: string, ...meta: any[]) => Logger;
 /**
- * updateProp updates property of an object in an immutable way.
- * @param k
- * @param v
- * @param state
- */
-export declare const updateProp: (k: string, v: any, state: {
-    [key: string]: any;
-}) => {
-    [key: string]: any;
-};
-/**
  * LambdaFunc define a type alias for lambda function.
  */
 export declare type LambdaFunc = (envs: Map<string, string | Environment | undefined>, logger: Logger, event: APIGatewayEvent) => Observable<any> | undefined;
+/**
+ * bool2Str convert a boolean value to string.
+ * @param k
+ */
+export declare const bool2Str: (v: boolean) => "false" | "true";
+/**
+ * boolPropToStr get a string representation value of bool within a object.
+ *
+ * @param k
+ */
+export declare const boolPropToStr: (k: string) => (x: any) => unknown;
+/**
+ * arrUnique takes an array as input and remove duplicates.
+ *
+ * @param array
+ */
+export declare const arrUnique: <T>(array: T[]) => T[];
+/**
+ * arrTrim trims every string elements in array.
+ *
+ * @param array
+ */
+export declare const arrTrim: (array: string[]) => string[];
+/**
+ * listUnique removes duplicates from immutable list.
+ *
+ * @param list
+ */
+export declare const listUnique: (list: immutable.List<any>) => immutable.List<any>;
+/**
+ * listRemoveEmpty removes empty values from an immutable list.
+ *
+ * @param list
+ */
+export declare const listRemoveEmpty: (list: immutable.List<any>) => immutable.List<any>;
+/**
+ * listTrim trims every elements in a immutable lit.
+ *
+ * @param list
+ */
+export declare const listTrim: (list: immutable.List<string>) => immutable.List<string>;
+/**
+ * mapHasKey checks if a immutable map has key.
+ *
+ * @param key
+ * @param map
+ */
+export declare const mapHasKey: (key: string, map: Map<string, any>) => boolean;
+/**
+ * mapGet gets value from immutable map by key.
+ *
+ * @param key
+ * @param map
+ */
+export declare const mapGet: (key: string, map: Map<string, any>) => any;
+/**
+ * mapSet sets value for immutable map.
+ *
+ * @param key
+ * @param map
+ * @param value
+ */
+export declare const mapSet: (key: string, map: Map<string, any>, value: any) => Map<string, any>;
--- operations.js
+++ operations.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Either_1 = require("fp-ts/lib/Either");
-const immer_1 = __importDefault(require("immer"));
 const sprintf_js_1 = require("sprintf-js");
+const lodash_1 = __importDefault(require("lodash"));
+const sanctuary_1 = __importDefault(require("sanctuary"));
+const function_1 = require("fp-ts/lib/function");
+// tslint:disable-next-line: no-var-requires
+const $ = require("sanctuary-def");
 /**
- * prefixDateTime prefix any string with date and time for consist displayingsuch as file ame.
+ * prefixDateTime prefix any string with date and time for consist displaying as file ame.
  * @param ft
  */
 exports.prefixDateTime = (ft) => (name, dateTime) => {
@@ -22,11 +26,66 @@ exports.prefixDateTime = (ft) => (name, dateTime) => {
  */
 exports.simpleLog = (logger, level, message, ...meta) => logger.log({ level, message, ...meta });
 /**
- * updateProp updates property of an object in an immutable way.
+ * bool2Str convert a boolean value to string.
  * @param k
- * @param v
- * @param state
  */
-exports.updateProp = (k, v, state) => immer_1.default(state, draft => {
-    draft[k] = v;
-});
+// @ts-ignore
+exports.bool2Str = (v) => v ? "true" : "false";
+/**
+ * boolPropToStr get a string representation value of bool within a object.
+ *
+ * @param k
+ */
+exports.boolPropToStr = (k) => sanctuary_1.default.pipe([sanctuary_1.default.prop(k), sanctuary_1.default.ifElse(sanctuary_1.default.is($.Boolean))(exports.bool2Str)(function_1.identity)]);
+/**
+ * arrUnique takes an array as input and remove duplicates.
+ *
+ * @param array
+ */
+exports.arrUnique = (array) => array.filter((a, b) => sanctuary_1.default.equals(array.indexOf(a))(b));
+/**
+ * arrTrim trims every string elements in array.
+ *
+ * @param array
+ */
+exports.arrTrim = (array) => array.map(lodash_1.default.trim);
+/**
+ * listUnique removes duplicates from immutable list.
+ *
+ * @param list
+ */
+exports.listUnique = (list) => list.toSet().toList();
+/**
+ * listRemoveEmpty removes empty values from an immutable list.
+ *
+ * @param list
+ */
+exports.listRemoveEmpty = (list) => list.filter((v) => !lodash_1.default.isEmpty(v));
+/**
+ * listTrim trims every elements in a immutable lit.
+ *
+ * @param list
+ */
+exports.listTrim = (list) => list.map((v) => lodash_1.default.trim(v));
+/**
+ * mapHasKey checks if a immutable map has key.
+ *
+ * @param key
+ * @param map
+ */
+exports.mapHasKey = (key, map) => map.has(key);
+/**
+ * mapGet gets value from immutable map by key.
+ *
+ * @param key
+ * @param map
+ */
+exports.mapGet = (key, map) => map.get(key);
+/**
+ * mapSet sets value for immutable map.
+ *
+ * @param key
+ * @param map
+ * @param value
+ */
+exports.mapSet = (key, map, value) => map.set(key, value);
