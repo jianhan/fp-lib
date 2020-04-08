2c3beab053099503eb7653038c54b381483e90cf
jianhan <jianhan@users.noreply.github.com>
2020-04-08 09:26:59 +0000

    deploy: 1d89b800d78003dc16e5f95ef28f6240baef51d8

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./constants";
-export * from "./envs";
-export * from "./logger";
-export * from "./logics";
-export * from "./operations";
--- logger.d.ts
+++
@@ -1,3 +0,0 @@
-import winston from "winston";
-import { Environment } from "./constants";
-export declare const createLogger: (environment: Environment, service: string, logLevel?: string) => winston.Logger;
